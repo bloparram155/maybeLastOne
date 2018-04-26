0ce6673994317fa10475b1133a5d4a41c43e8624
blopar <pablo.parram155@gmail.com>
2018-04-25 22:57:00 -0800

    Controlador:Home. Views:cursos,galeria,promociones,servicios,conocenos. Assets:customCss

---
+++ application/views/home/promociones.php
@@ -0,0 +1,97 @@
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-lg-3">
+
+          <h1 class="my-4">Promociones</h1>
+          <div class="list-group">
+            <a href="cursos.html" class="list-group-item">Cursos</a>
+            <a href="Promos.html" class="list-group-item">Promociones</a>
+            <a href="servicios.html" class="list-group-item">Servicios</a>
+            <a href="galeria.html" class="list-group-item">Galeria</a>
+          </div>
+
+        </div>
+        <!-- /.col-lg-3 -->
+
+        <div class="col-lg-9">
+
+          <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+              <div class="carousel-item active">
+                <img class="d-block img-fluid" src="assets/img/auto.png" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="assets/img/glitterbanner.jpg" alt="Second slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="assets/img/peinado.png" alt="Third slide">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
+          <div class="row">
+
+            <div class="col-lg-4 col-md-6 mb-4">
+              <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="assets/img/corte.png" alt=""></a>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    <a href="#">Tratamiento Gratis</a>
+                  </h4>
+                  <h5>Tratamiento Gratis</h5>
+                  <h6>En Corte de Cabello</h6>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mb-4">
+              <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="assets/img/horaFeliz.png" alt=""></a>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    <a href="#">Hora Feliz</a>
+                  </h4>
+                  <h5>De 8am a 11am</h5>
+                  <h6>Precio especial en servicios</h6>
+                </div>
+              </div>
+            </div>
+            
+            <div class="col-lg-4 col-md-6 mb-4">
+              <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="assets/img/corteGratis.png" alt=""></a>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    <a href="#">Corte Gratis</a>
+                  </h4>
+                  <h5>En Servicio de Tinte y Color</h5>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <!-- /.row -->
+
+        </div>
+        <!-- /.col-lg-9 -->
+
+      </div>
+      <!-- /.row -->
+
+    </div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,35 +1,60 @@
-<html>
-        <head>
-                <title>Glitters Hair & Make Up Studio</title>
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-        </head>
-        <body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Glitters Hair & Make Up Studio</title>
 
-			 <nav class="navbar navbar-expand-lg navbar-inverse">
-			 	<a class="navbar-brand" href="/">Glitters</a>
-			 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-			       <span class="navbar-toggler-icon"></span>
-			    </button>
-			 	<div class="container">
-			 		<div class="collapse navbar-collapse" id="navbarNavDropdown">
-			 			<ul class="navbar-nav">
-			 				<li class="nav-item active"><a href="<?php echo base_url(); ?>">Home</a></li>
-			 				<li><a href="<?php echo base_url(); ?>/conocenos">Conocenos</a></li>
-			 				<li><a href="/">Galeria</a></li>
-			 				<li class="nav-item dropdown">
-			 					<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="/">Dropdown Link</a>
-								<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-									<a class="dropdown-item" href="#">Item</a>
-									<a class="dropdown-item" href="#">Item</a>
-									<a class="dropdown-item" href="#">Item</a>
-									<a class="dropdown-item" href="#">Item</a>
-								</div>
-			 				</li>
-			 			</ul>
-			 		</div>
-			 	</div>
-			 </nav>	
-			 <section class="container">
-			 	
+    <!-- Bootstrap -->
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!--CustomCss-->
+    <link href="assets/bootstrap/customCss/customCss.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+  	<!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="Home">Glitters Hair & Make Up Studio</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="Home">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		          Especialidades
+		        </a>
+		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          <a class="dropdown-item" href="Cursos">Cursos</a>
+		          <div class="dropdown-divider"></div>
+		          <a class="dropdown-item" href="Promociones">Promociones</a>
+		          <div class="dropdown-divider"></div>
+		          <a class="dropdown-item" href="Servicios">Servicios</a>
+		        </div>
+	        </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Conocenos">Conocenos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Galeria">Galeria</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+	</nav>
+
+
+
+
 			
              
